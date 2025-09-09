0999be5d065897371dcb86212608217e5f502ce9
aleks1303 <aleksbulda13@gmail.com>
2025-09-09 22:40:48 +0300

    express

--- index.js
+++ index.js
@@ -1,114 +1,114 @@
 const express = require("express")
 const app = express()
+const path = require('node:path');
+const fsPromises = require('node:fs/promises');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
-const users = [
-    {id: 1, name: 'vasya', age: 23, email: 'vas31@gmail.com', password: 'qwe233'},
-    {id: 2, name: 'petya', age: 34, email: 'pet30@gmail.com', password: '233eds'},
-    {id: 3, name: 'kolya', age: 53, email: 'kolya29@gmail.com', password: 'ddf3455'},
-    {id: 4, name: 'olga', age: 13, email: 'olga28@gmail.com', password: 'red3455'},
-    {id: 5, name: 'max', age: 33, email: 'max30@gmail.com', password: '43ew234'},
-    {id: 6, name: 'anya', age: 27, email: 'anya31@gmail.com', password: '4e3w2'},
-    {id: 7, name: 'oleg', age: 28, email: 'oleg28@gmail.com', password: '3we4r'},
-    {id: 8, name: 'andrey', age: 43, email: 'andrey29@gmail.com', password: 'dfr43'},
-    {id: 9, name: 'masha', age: 33, email: 'masha30@gmail.com', password: '432werf'},
-    {id: 10, name: 'olga', age: 41, email: 'olya31@gmail.com', password: '432dfg'},
 
-];
+const usersFile = path.join(__dirname, 'users.json')
 
-app.get('/users', (req, res) => {
-    try{
+const userWrite = async (users) => {
+    await fsPromises.writeFile(usersFile, JSON.stringify(users, null, 2), "utf-8")
+};
+
+const userRead = async () => {
+    try {
+        const data = await fsPromises.readFile(usersFile, "utf-8")
+        return JSON.parse(data)
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            await fsPromises.writeFile(usersFile, JSON.stringify([], null, 2), "utf-8")
+            return []
+        }
+    }
+};
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await userRead()
         res.send(users)
     } catch (e) {
-        res.status(500).send(e.message)
+       return  res.status(500).send(e.message)
     }
 });
-app.post('/users', (req, res) => {
-    try {
-        const {name, email, password} = req.body;
-        if (name.length < 3) {
-            res.send('Name is low')
-        }
-        if (email === !'@') {
-            res.send('Email wrong')
-        }
-        if (password < 1 && password > 20) {
-            res.send('Password wrong')
-        }
-        const id = users[users.length - 1].id + 1;
-        const newUser = {id, name, email, password};
-        users.push(newUser);
-        res.status(201).send(newUser)
-    }
-    catch (e){
-        res.send(500).send(e.message)
-    }
 
-});
-app.get('/users/:userId', (req, res) => {
+app.post('/users', async (req, res) => {
     try {
+        const {name, age, email, password} = req.body;
+        const users = await userRead()
+        if (name.length < 3) {
+         return  res.send('Name is low')
+        }
+        if (age <= 0) {
+           return  res.send('age wrong')
+        }
+        const id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+        const newUser = {id, name, age, email, password};
+        users.push(newUser);
+        await userWrite(users)
+        res.status(201).send(newUser)
+    } catch (e) {
+       return res.status(500).send(e.message)
+    }
+});
+
+app.get('/users/:userId', async (req, res) => {
+    try {
+
         const userId = Number(req.params.userId);
+        const users = await userRead()
         const user = users.find(user => user.id === userId);
         if (!user) {
-            res.status(404).send('User Not found')
+           return  res.status(404).send('User Not found')
         }
-        res.send(user)
-        console.log(userId)
+       return res.send(user)
+
     } catch (e) {
         res.status(500).send(e.message)
     }
 });
 
-app.put('/users/:userId', (req, res) => {
-    try{
+app.put('/users/:userId', async (req, res) => {
+    try {
+
         const userId = Number(req.params.userId);
+        const users = await userRead();
         const userIndex = users.findIndex(user => user.id === userId);
-        if (userIndex === -1){
+        if (userIndex === -1) {
             return res.status(404).send('User not found')
         }
-        const {name, email, password} = req.body;
+        const {name, age, email, password} = req.body;
         users[userIndex].name = name;
+        users[userIndex].age = age;
         users[userIndex].email = email;
         users[userIndex].password = password;
+        await userWrite(users)
         res.status(201).send(users[userIndex])
+    } catch (e) {
+       return  res.status(500).send(e.message)
     }
-    catch (e){
-        res.status(500).send(e.message)
-    }
-})
+});
 
-app.delete('/users/:userId', (req, res) => {
-    try{
-        const userId = Number(req.params.userId)
+app.delete('/users/:userId', async (req, res) => {
+    try {
+
+        const userId = Number(req.params.userId);
+        const users = await userRead()
         const userIndex = users.findIndex(user => user.id === userId)
         if (userIndex === -1) {
-            res.status(404).send('User not found')
+           return  res.status(404).send('User not found')
         }
         users.splice(userIndex, 1)
+        await userWrite(users)
         res.sendStatus(204)
-    }
-    catch (e){
-        res.status(500).send(e.message)
+    } catch (e) {
+       return  res.status(500).send(e.message)
     }
 });
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000/users')
 });
 
-
-// Текстовий урок
-// BE. NodeJS HW3
-// Закінчити з CRUD операціями.
-//
-//     При створенні робити валідацію на імʼя і вік,
-//
-//     імʼя повинно бути більше за 3 символи, вік – не менше нуля
-//
-// На гет, пут, деліт юзерів перевірити чи такий юзер є в базі.
-//
-//     якщо немає – вивести помилку
-//
-// Використовуйте шляхи для нових ендпоінтів згідно REST правил
 
