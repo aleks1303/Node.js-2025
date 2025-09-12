e79d4397b2a59f57c357e214d5926e50b3556808
aleks1303 <aleksbulda13@gmail.com>
2025-09-12 14:06:55 +0300

    master

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,22 +1,26 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const read = async () => {
-    try {
-        const pathToFile = path.join(__dirname, 'db.json');
-        const data = await fs.readFile(pathToFile, 'utf-8');
-        return data ? JSON.parse(data) : [];
-    } catch (e) {
-        console.log("Помилка запису", e.message);
-    }
-};
+
 
 const write = async (users) => {
     try {
-        const pathToFile = path.join(__dirname, 'db.json');
+        const pathToFile = path.join(process.cwd(), 'db.json');
         await fs.writeFile(pathToFile, JSON.stringify(users));
     }
     catch (e) {
+        console.log("Помилка запису", e.message);
+    }
+};
+const read = async () => {
+    try {
+        const pathToFile = path.join(process.cwd(), 'db.json');
+        const data = await fs.readFile(pathToFile, 'utf-8');
+        return data ? JSON.parse(data) : [];
+    } catch (e) {
+        if (e.code === "ENOENT") {
+            await fs.writeFile(pathToFile, JSON.stringify(users));
+        }
         console.log("Помилка запису", e.message);
     }
 };
