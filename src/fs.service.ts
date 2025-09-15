b724f82ef03df6ae05806bb0a2fa93b2ea634c06
aleks1303 <aleksbulda13@gmail.com>
2025-09-15 19:44:48 +0300

    lesson 4

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,30 +1,29 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import {IUser} from "./interfaces/user.interface";
+import fs from "node:fs/promises";
+import path from "node:path";
 
-const pathToFile = path.join(process.cwd(), 'db.json');
+import { IUser } from "./interfaces/user.interface";
 
-const read = async ():Promise<IUser[]> => {
-    try {
-        const data = await fs.readFile(pathToFile, 'utf-8');
-        return data ? JSON.parse(data) : [];
-    } catch (e) {
-        if (e.code === "ENOENT") {
-            await fs.writeFile(pathToFile, JSON.stringify([], null, 2), "utf-8");
-            return [];
-        }
-        console.log("Помилка запису", e.message);
+const pathToFile = path.join(process.cwd(), "db.json");
+
+const read = async (): Promise<IUser[]> => {
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    return data ? JSON.parse(data) : [];
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      await fs.writeFile(pathToFile, JSON.stringify([], null, 2), "utf-8");
+      return [];
     }
+    console.log("Помилка запису", e.message);
+  }
 };
 
-const write = async (users:IUser[]):Promise<void> => {
-    try {
-        await fs.writeFile(pathToFile, JSON.stringify(users, null, 2), 'utf-8');
-    } catch (e) {
-        console.log("Помилка запису", e.message);
-    }
+const write = async (users: IUser[]): Promise<void> => {
+  try {
+    await fs.writeFile(pathToFile, JSON.stringify(users, null, 2), "utf-8");
+  } catch (e) {
+    console.log("Помилка запису", e.message);
+  }
 };
 
-
-
-export {read, write};
+export { read, write };
