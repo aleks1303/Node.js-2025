253046b1a671ed80ad3c2e627e2230052ede67b8
aleks1303 <aleksbulda13@gmail.com>
2025-09-17 15:15:50 +0300

    homework 4

--- src/controllers/user.contoller.ts
+++ src/controllers/user.contoller.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 
+import { userService } from "../services/user.service";
+
 export const userController = {
   getAllUsers: async (res: Response, req: Request, next: NextFunction) => {
-    const users = await userService.getAllUsers();
-    res.json(users);
+    try {
+      const users = await userService.getAllUsers();
+      res.json(users);
+    } catch (e) {
+      next(e);
+    }
   },
 };
