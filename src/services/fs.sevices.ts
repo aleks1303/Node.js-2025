14e8aa39b821341d60d5dd25b893869f0498288e
aleks1303 <aleksbulda13@gmail.com>
2025-09-17 14:40:18 +0300

    homework 4

--- src/services/fs.sevices.ts
+++ src/services/fs.sevices.ts
@@ -4,10 +4,19 @@ import path from "node:path";
 const pathToFile = path.join(process.cwd(), "db.json");
 
 const read = async () => {
-  const data = await fs.readFile(pathToFile, "utf-8");
-  return data ? JSON.parse(data) : [];
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    return data ? JSON.parse(data) : [];
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      await fs.writeFile(pathToFile, JSON.stringify([], null, 2), "utf-8");
+      return [];
+    }
+  }
 };
 
-const write = () => {};
+const write = async () => {
+  await fs.writeFile(pathToFile, )
+};
 
 export { read, write };
