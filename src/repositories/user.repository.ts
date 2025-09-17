a41eb79ecada7353d7dbdfacf91eb40025711cbb
aleks1303 <aleksbulda13@gmail.com>
2025-09-17 15:23:26 +0300

    homework 4

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+import { IUser } from "../interfaces/user.interface";
+import { read } from "../services/fs.sevices";
+
+export const userRepository = {
+  getAllUsers: async (): Promise<IUser[]> => {
+    return await read();
+  },
+};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,8 @@
+import { IUser } from "../interfaces/user.interface";
+import { userRepository } from "../repositories/user.repository";
+
 export const userService = {
-  getAllUsers: async () => {
-    await userRepository.getAllUsers();
+  getAllUsers: async (): Promise<IUser[]> => {
+    return await userRepository.getAllUsers();
   },
 };
