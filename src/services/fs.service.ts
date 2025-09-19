fb24df0fdb25e7ad06aab9858bdc3eeceb375fd1
aleks1303 <aleksbulda13@gmail.com>
2025-09-19 14:16:36 +0300

    homework 5

--- src/services/fs.service.ts
+++ src/services/fs.service.ts
@@ -6,8 +6,16 @@ import { IUser } from "../interfaces/user.interface";
 const pathToFile = path.join(process.cwd(), "db.json");
 
 const read = async (): Promise<IUser[]> => {
-  const data = await fs.readFile(pathToFile, "utf-8");
-  return data ? JSON.parse(data) : [];
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    return data ? JSON.parse(data) : [];
+  } catch (e) {
+    console.log("Помилка завантаження", e.message);
+  }
 };
 
-const write = async (users: IUser[]): Promise<void> => {};
+const write = async (users: IUser[]): Promise<void> => {
+  try{
+    await fs.writeFile(pathToFile, JSON.stringify(users, null, 2), "utf-8")
+  }
+};
