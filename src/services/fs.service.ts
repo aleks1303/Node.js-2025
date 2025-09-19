1967224e3d075c8ae3206de1a1c8a7c83b1efc6a
aleks1303 <aleksbulda13@gmail.com>
2025-09-19 14:13:34 +0300

    homework 5

--- src/services/fs.service.ts
+++ src/services/fs.service.ts
@@ -1,9 +1,13 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
+import { IUser } from "../interfaces/user.interface";
+
 const pathToFile = path.join(process.cwd(), "db.json");
 
-const read = async () => {
+const read = async (): Promise<IUser[]> => {
   const data = await fs.readFile(pathToFile, "utf-8");
-  return data ? JSON.parse(data) : []
+  return data ? JSON.parse(data) : [];
 };
+
+const write = async (users: IUser[]): Promise<void> => {};
