b9aea58743fccc9651867d2c845ef714f02da6e5
aleks1303 <aleksbulda13@gmail.com>
2025-09-22 19:54:47 +0300

    homework 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/users", userRouter);
+
+app.listen(3000, () => {
+  console.log("We are listening on port 3000");
+});
