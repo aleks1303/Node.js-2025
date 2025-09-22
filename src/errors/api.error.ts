8478c611e74abf74c7d9989c5c6441b06529429f
aleks1303 <aleksbulda13@gmail.com>
2025-09-22 21:12:04 +0300

    homework 5

---
+++ src/errors/api.error.ts
@@ -0,0 +1,10 @@
+// class ApiError extends Error {
+//   constructor(
+//     public message: string,
+//     public status: number,
+//   ) {
+//     super(message);
+//     this.status = status;
+//   }
+// }
+// export const apiError = new ApiError();
