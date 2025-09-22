243a6c2e37ee4273dc580e5863cd370c2092554e
aleks1303 <aleksbulda13@gmail.com>
2025-09-22 21:22:54 +0300

    homework 5

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import * as mongoose from "mongoose";
 
 import { config } from "./configs/config";
@@ -11,8 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use("/users", userRouter);
 
-app.use((error: ApiError, req: Request, res: Response, next: NextFunction) => {
+app.use((error: ApiError, req: Request, res: Response) => {
   res.status(error.status || 500).send(error.message);
+});
+
+process.on("uncaughtException", (error) => {
+  console.log("uncaughtException", error.message, error.stack);
 });
 
 const port = config.APP_PORT;
