8d6f7e34d6b728019130156a5b539fd779642244
aleks1303 <aleksbulda13@gmail.com>
2025-09-22 21:01:37 +0300

    homework 5

---
+++ src/models/user.model.ts
@@ -0,0 +1,14 @@
+// import { Schema } from "mongoose";
+//
+// import { RoleEnum } from "../enums/user.enum";
+//
+// const userSchema = new Schema({
+//   name: { type: String, required: true },
+//   age: { type: Number, required: true },
+//   email: { type: String, required: false, unique: true },
+//   password: { type: String, required: true },
+//   phone: { type: String, required: false },
+//   role: { type: String, enum: RoleEnum },
+// });
+//
+// export const User = model<IUser>
