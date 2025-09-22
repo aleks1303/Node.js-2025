5bf446627e5b7b39e30b7c52c3b71b232d68ad7a
aleks1303 <aleksbulda13@gmail.com>
2025-09-22 20:26:15 +0300

    homework 5

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import * as mongoose from "mongoose";
+
+import { config } from "./configs/config";
 
 const app = express();
 
@@ -7,6 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use("/users", userRouter);
 
-app.listen(3000, () => {
-  console.log("We are listening on port 3000");
+const port = config.APP_PORT;
+const host = config.APP_HOST;
+const mongodb = config.MONGO_URI;
+
+app.listen(port, () => {
+  mongoose.connect(mongodb);
+  console.log(`We are listening on ${host}: ${port}`);
 });
