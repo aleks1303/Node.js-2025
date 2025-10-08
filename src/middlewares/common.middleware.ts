33bb95a5ab8ef46fc6134097f8c85f573f7d669e
aleks1303 <aleksbulda13@gmail.com>
2025-10-08 12:42:12 +0300

    homework 6

--- src/middlewares/common.middleware.ts
+++ src/middlewares/common.middleware.ts
@@ -1,11 +1,18 @@
-// import { NextFunction, Request, Response } from "express";
-//
-// class CommonMiddleware {
-//   public isIdValid(req: Request, res: Response, next: NextFunction) {
-//     try {
-//     } catch (e) {
-//       next(e);
-//     }
-//   }
-// }
-// export const commonMiddleware = new CommonMiddleware();
+import { NextFunction, Request, Response } from "express";
+import { isObjectIdOrHexString } from "mongoose";
+
+import { ApiError } from "../errors/api.error";
+
+class CommonMiddleware {
+  public isIdValid(key: string) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      if (!isObjectIdOrHexString(req.params[key])) {
+        throw new ApiError("Id is not valid", 409);
+      }
+      next();
+    };
+  }
+
+  public isBodyValid() {}
+}
+export const commonMiddleware = new CommonMiddleware();
