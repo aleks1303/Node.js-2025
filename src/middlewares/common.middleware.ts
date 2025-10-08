23e5923a9b699757be337d5ea553a202eff1befd
aleks1303 <aleksbulda13@gmail.com>
2025-10-08 12:36:38 +0300

    homework 6

---
+++ src/middlewares/common.middleware.ts
@@ -0,0 +1,11 @@
+// import { NextFunction, Request, Response } from "express";
+//
+// class CommonMiddleware {
+//   public isIdValid(req: Request, res: Response, next: NextFunction) {
+//     try {
+//     } catch (e) {
+//       next(e);
+//     }
+//   }
+// }
+// export const commonMiddleware = new CommonMiddleware();
