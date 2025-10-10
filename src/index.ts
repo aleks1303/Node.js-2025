b3fce906ee83d277d1a6f9798b079ad2c5b3cf78
aleks1303 <aleksbulda13@gmail.com>
2025-10-10 17:05:59 +0300

    lesson 7 nodemailer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import express, { NextFunction, Request, Response } from "express";
+import * as mongoose from "mongoose";
 
+import { config } from "./configs/config";
+import { ApiError } from "./errors/api.error";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).send(err.message);
+});
+
+process.on("uncaughtException", (error) => {
+  console.log("uncaughtException", error.message, error.stack);
+});
+
+const port = config.APP_PORT;
+const host = config.APP_HOST;
+const mongoDb = config.MONGO_URI;
+app.listen(port, async () => {
+  await mongoose.connect(mongoDb);
+  console.log(`Server started on http://${host}:${port} `);
+});
