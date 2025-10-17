9f29b7a00c9e248ef3aa03410e7e41aa467f288c
aleks1303 <aleksbulda13@gmail.com>
2025-10-17 12:30:19 +0300

    homework 7 nodemailer add logout

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,8 @@
+import { IUser } from "../interfaces/user.interface";
+import { userRepository } from "../repositories/user.repository";
+
 class UserService {
-  public async getAllUsers() {
+  public async getAllUsers(): Promise<IUser[]> {
     return await userRepository.getAllUsers();
   }
 }
