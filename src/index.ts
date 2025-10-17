cfcf1e1f651af9d133cb925a57e5a7994ee000bd
aleks1303 <aleksbulda13@gmail.com>
2025-10-17 11:42:45 +0300

    homework 7 nodemailer add logout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import express, { NextFunction, Request, Response } from "express";
 
+import { ApiError } from "./errors/api.error";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).send(err.message);
+});
+
+process.on("uncaughtException", (error) => {
+  console.log("uncaughtException", error.message, error.stack);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
