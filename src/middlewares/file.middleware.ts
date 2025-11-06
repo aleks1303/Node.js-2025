338aa37ee87104ad20b7f042f3357356fb844e41
aleks1303 <aleksbulda13@gmail.com>
2025-11-06 19:48:49 +0200

    lesson 10 aws_s3 presenters

--- src/middlewares/file.middleware.ts
+++ src/middlewares/file.middleware.ts
@@ -1,14 +1,29 @@
 import { NextFunction, Request, Response } from "express";
+import { UploadedFile } from "express-fileupload";
+
+import { ApiError } from "../errors/api.error";
 
 class FileMiddleware {
-  public isFileValid() {
+  private maxSize = 5 * 1024 * 1024;
+  private mimes = ["image/jpeg", "image/png", "image/jpg"];
+  public isFileValid = () => {
     return (req: Request, res: Response, next: NextFunction) => {
       try {
+        if (!req.files || !req.files.avatar) {
+          throw new ApiError("File is not exist", 400);
+        }
+        const file = req.files.avatar as UploadedFile;
+        if (file.size > this.maxSize) {
+          throw new ApiError("large file allowed up to 5Mb", 400);
+        }
+        if (!this.mimes.includes(file.mimetype)) {
+          throw new ApiError(`invalid file type, require${this.mimes}`, 400);
+        }
         next();
       } catch (e) {
         next(e);
       }
     };
-  }
+  };
 }
 export const fileMiddleware = new FileMiddleware();
